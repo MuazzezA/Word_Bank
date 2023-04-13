f8c0278e9224a26eb0e76f76f1334b0ff9b2d92e
Muazzez Aydın <ayd.muazzez@gmail.com>
2023-04-13 22:31:48 +0300

    Edit: Bottom navigation style

--- app/navigation/bottom-nav.js
+++ app/navigation/bottom-nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import WordStack from './word-stack';
 import HomeStack from './home-stack';
 import ProfileStack from './profile-stack';
+import {sizes} from '../util/sizing';
 const Tab = createBottomTabNavigator();
 
 export default BottomNav = () => {
@@ -11,6 +12,8 @@ export default BottomNav = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.bottomContainer,
       }}>
       <Tab.Screen name="home-stack" component={HomeStack} />
       <Tab.Screen name="word-stack" component={WordStack} />
@@ -18,3 +21,11 @@ export default BottomNav = () => {
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  bottomContainer: {
+    paddingTop: sizes.base,
+    backgroundColor: '#fff',
+    height: sizes.bottomTabHeight,
+  },
+});
