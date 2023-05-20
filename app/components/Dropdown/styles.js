f1ff49b2f376176fd9acc8cfabd55001b4570758
MuazzezA <ayd.muazzez@gmail.com>
2023-05-20 23:38:18 +0300

    create: dropdown component

---
+++ app/components/Dropdown/styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import {colors, sizes} from '../../util';
+
+export default StyleSheet.create({
+  dropdownContainer: {
+    backgroundColor: 'transparent',
+    marginHorizontal: 10,
+  },
+  selectedItem: {backgroundColor: colors.pompAndPower},
+  selectedItemText: {color: colors.white},
+});
