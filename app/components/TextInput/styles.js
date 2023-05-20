6576b84af60f0b71e3c371e6a062fa1d9c1aa48a
MuazzezA <ayd.muazzez@gmail.com>
2023-05-20 22:41:24 +0300

    Create: text input component

---
+++ app/components/TextInput/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import {colors, sizes} from '../../util';
+
+export default StyleSheet.create({
+  input: {
+    marginHorizontal: 10,
+    backgroundColor: colors.platinum,
+  },
+  outline: {
+    borderRadius: sizes.radius,
+  },
+});
