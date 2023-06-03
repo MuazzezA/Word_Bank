9289a98170b2f9a2aa32c0dede4d38cbc2ddb55b
MuazzezA <ayd.muazzez@gmail.com>
2023-06-03 23:11:48 +0300

    :package: remove react-native-paper

--- app/App.js
+++ app/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {Provider as PaperProvider} from 'react-native-paper';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import theme from './util/theme';
 import AppStack from './navigation/app-stack';
 
 export default function App() {
   return (
     // Wrap your app in the Store component
-    <PaperProvider theme={theme}>
-      <SafeAreaView style={styles.container}>
-        <AppStack />
-      </SafeAreaView>
-    </PaperProvider>
+    <SafeAreaView style={styles.container}>
+      <AppStack />
+    </SafeAreaView>
   );
 }
 
